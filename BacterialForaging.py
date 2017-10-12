fb2ec3fe6f071deee1a821c2338fb93bd1db7a3b
TT13 <taran13t@gmail.com>
2017-10-12 09:33:17 +0300

    added test functions call

---
+++ BacterialForaging.py
@@ -0,0 +1,5 @@
+from TestFunctions import get_function
+
+f = get_function()
+
+print(f([0, 1]))
---
+++ TestFunctions.py
@@ -0,0 +1,13 @@
+import random
+import  math
+
+functions_name = ["Easom’s function"]
+
+#calls function with corresponding index
+def get_function(n = random.randint(0, len(functions_name) - 1)):
+    print(functions_name[n])
+    return globals()['f'+str(n)]
+
+def f0(x):
+    return -math.cos(x[0]) * math.sin(x[1])*math.exp(-(x[0]-math.pi)**2+((x[1]-math.pi)**2))
+

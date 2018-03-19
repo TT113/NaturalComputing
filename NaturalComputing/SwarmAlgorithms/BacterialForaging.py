d15f61f39ac02bf3e97f062dd2583626041db034
tt13 <taran13t@gmail.com>
2018-03-19 20:17:10 +0200

    fixes

--- NaturalComputing/SwarmAlgorithms/BacterialForaging.py
+++ NaturalComputing/SwarmAlgorithms/BacterialForaging.py
@@ -1,36 +1,29 @@
-from NaturalComputing.Models.Entity import Entity
-from NaturalComputing.TestFunctions import get_function
-from NaturalComputing.Models.Population import Population
-
-
 import numpy as np
 import random as random
 import math as math
 import operator
 
-best = 100000000000000000
+from NaturalComputing.Models.Entity import Entity
+from NaturalComputing.TestFunctions import get_function
 
 
 class Bacteria(Entity):
 
     def __new__(cls, coordinates_in_space):
         obj = Entity.__new__(cls, coordinates_in_space)
-        obj.health = 0.0
-        obj.fitness = 0.0
-        obj.cost = 0.0
         return obj
 
     def __init__(self, coordinates_in_space):
+        self.health = 0.0
+        self.fitness = 0.0
+        self.cost = 0.0
         self.calc_cost()
 
     def __str__(self):
         return super().__str__()
 
     def calc_cost(self):
-        global best
         self.cost = searching_function(self)
-        if self.cost < best:
-            best = self.cost
 
 
 search_space_dimension = 2
@@ -39,13 +32,13 @@ number_of_bacteria_in_population = 50
 
 number_of_split = int(number_of_bacteria_in_population / 2)
 
-number_of_elimination_dispersal_events = 5
-number_of_reproduction_steps = 5
-number_of_chemotaxis_steps = 100
+number_of_elimination_dispersal_events = 3
+number_of_reproduction_steps = 6
+number_of_chemotactic_steps = 20
 
 elimination_dispersal_probability = 0.25
 
-swimming_length = 3
+swimming_length = 4
 step_size = 0.1
 
 attractant_depth = 0.1
@@ -105,25 +98,16 @@ def evaluate(bacteria, population, d_attr, w_attr, h_rep, w_rep):
 
 def tumble_cell(bacteria, search_space):
     step = generate_random_direction(search_space_dimension)
+    location_in_space = np.zeros(search_space_dimension)
     for i in range(0, search_space_dimension):
         coordinate = bacteria[i] + step_size * step[i]
         if coordinate < search_space[i][0]:
             coordinate = search_space[i][0]
         elif coordinate > search_space[i][1]:
             coordinate = search_space[i][1]
-        bacteria[i] = coordinate
-    bacteria.step = step
-    return bacteria
+        location_in_space[i] = coordinate
+    return Bacteria(location_in_space)
 
-
-def swim_step(new_cell, current_cell, search_space):
-    for i in range(0, search_space_dimension):
-        coordinate = new_cell[i] + step_size * new_cell.step
-        if coordinate < search_space[i][0]:
-            coordinate = search_space[i][0]
-        elif coordinate > search_space[i][1]:
-            coordinate = search_space[i][1]
-        new_cell[i] = coordinate
 
 def check_for_best_solution(bacteria, best_bacteria):
     if best_bacteria is None or bacteria.cost < best_bacteria.cost:
@@ -131,15 +115,16 @@ def check_for_best_solution(bacteria, best_bacteria):
     return best_bacteria
 
 
-def chemotaxis(population, search_space):
+def chemotactic(population, search_space):
     best_bacteria = None
-    for i in range(0, number_of_chemotaxis_steps):
+    for i in range(0, number_of_chemotactic_steps):
         moved_cells = []
         for bacteria in population:
-            sum_of_nutrients = 0
-            evaluate(bacteria, population, attractant_depth, attractant_signal_width, repellent_effect_height, repellent_effect_width)
+            health = 0
+            evaluate(bacteria, population, attractant_depth, attractant_signal_width, repellent_effect_height,
+                     repellent_effect_width)
             best_bacteria = check_for_best_solution(bacteria, best_bacteria)
-            sum_of_nutrients += bacteria.fitness
+            health += bacteria.fitness
             for j in range(0, swimming_length):
                 new_cell = tumble_cell(bacteria, search_space)
                 evaluate(new_cell, population, attractant_depth, attractant_signal_width, repellent_effect_height,
@@ -148,11 +133,11 @@ def chemotaxis(population, search_space):
                 if new_cell.fitness > bacteria.fitness:
                     break
                 bacteria = new_cell
-                sum_of_nutrients += bacteria.fitness
-            bacteria.health = sum_of_nutrients
+                health += bacteria.fitness
+            bacteria.health = health
             moved_cells.append(bacteria)
         population = moved_cells
-        # print(best_bacteria.fitness + best_bacteria.cost)
+        # print(str(best_bacteria.fitness) + " " + str(best_bacteria.cost))
     return [best_bacteria, population]
 
 
@@ -163,8 +148,7 @@ def reinit_population(population):
 
 def reproduction(population):
     best_bacteria = sorted(population, key=operator.attrgetter('health'), reverse=True)[:int(len(population) / 2)]
-    population = best_bacteria + best_bacteria
-    reinit_population(population)
+    return best_bacteria + best_bacteria
 
 
 def elimination_dispersal(population, search_space):
@@ -187,13 +171,13 @@ def locate__new_population_randomly_in_space(search_space):
 
 def bacterial_foraging_algorithm():
     best_bacteria = None
-    search_space = generate_space(np.array([-100, 100]), search_space_dimension)
+    search_space = generate_space(np.array([-10, 10]), search_space_dimension)
     population = locate__new_population_randomly_in_space(search_space)
     for i in range(0, number_of_elimination_dispersal_events):
         for j in range(0, number_of_reproduction_steps):
-            best_for_chemotaxis = chemotaxis(population, search_space)[0]
-            best_bacteria = check_for_best_solution(best_for_chemotaxis, best_bacteria)
-            reproduction(population)
+            best_for_chemotactic = chemotactic(population, search_space)[0]
+            best_bacteria = check_for_best_solution(best_for_chemotactic, best_bacteria)
+            population = reproduction(population)
         elimination_dispersal(population, search_space)
     print(best_bacteria.cost)
 
--- NaturalComputing/TestFunctions.py
+++ NaturalComputing/TestFunctions.py
@@ -5,7 +5,7 @@ functions_name = ["Sum of squares", "Easom’s function"]
 
 
 # calls function with corresponding index
-def get_function(n = random.randint(0, len(functions_name) - 2)):
+def get_function(n = random.randint(0, len(functions_name) - 1)):
     print(functions_name[n])
     return globals()['f'+str(n)]
 
@@ -17,8 +17,8 @@ def f0(x):
         sum += x[i]**2
     return sum
 
-#
-# def f1(x):
-#     return -math.cos(x[0]) * math.sin(x[1]) * math.exp(-(x[0] - math.pi) ** 2 + ((x[1] - math.pi) ** 2))
+
+def f1(x):
+    return -math.cos(x[0]) * math.sin(x[1]) * math.exp(-(x[0] - math.pi) ** 2 + ((x[1] - math.pi) ** 2))
 
 
